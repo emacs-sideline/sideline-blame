94b35e18a95c00f1122adfc30b24ad3ed32329be
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-26 20:36:02 +0800

    update keywords

--- sideline-blame.el
+++ sideline-blame.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/emacs-sideline/sideline-blame
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "27.1") (sideline "0.1.0") (vc-msg "1.1.1"))
-;; Keywords: sideline blame
+;; Keywords: convenience blame
 
 ;; This file is not part of GNU Emacs.
 
